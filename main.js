96903c3958ffb6c2bbf7a4f08dff04235c0c3dd2
Jakub Pietrasik <jakubmkjp@gmail.com>
2023-12-01 20:24:33 +0100

    scenes division

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0,0,1);
-const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+let currentScene = 'main';
+const scenes = {
+    main: new THREE.Scene(),
+    game: new THREE.Scene()
+};
+
+const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 1);
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
@@ -15,14 +23,68 @@ controls.maxDistance = 50;
 controls.autoRotate = true;
 controls.autoRotateSpeed = -0.2;
 controls.enablePan = false;
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 let cameraAnimating = true;
+
+function initMainScene() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    scenes.main.add(ambientLight);
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('halemba_model.gltf', (gltf) => {
+    const root = gltf.scene;
+    root.position.set(0, 0, 0);
+    scenes.main.add(root);
+    cameraAnimating = true;
+    });
+   
+}
+
+function initGameScene() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    scenes.game.add(ambientLight);
+
+   
+}
+
+function switchScene(sceneName) {
+    currentScene = sceneName;
+    camera.position.set(0, 0, 1); // Resetuj pozycję kamery
+    controls.reset(); // Resetuj kontrolery
+
+    if (sceneName === 'game') {
+        initGameScene();
+    } else {
+        initMainScene();
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+
+    if (currentScene === 'main' && cameraAnimating) {
+        // Animacja kamery dla sceny głównej
+        camera.position.z += 0.03;
+        camera.position.x += 0.03;
+        camera.position.y += 0.03;
+        if (camera.position.z > 12) {
+            cameraAnimating = false; 
+            showText();
+        }
+    }
+
+    renderer.render(scenes[currentScene], camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 function showText() {
     const textDiv = document.createElement('div');
-    textDiv.classList.add('animated-text'); 
+    textDiv.classList.add('animated-text');
     textDiv.style.position = 'absolute';
     textDiv.style.top = '40px';
     textDiv.style.width = '100%';
@@ -34,57 +96,27 @@ function showText() {
     document.body.appendChild(textDiv);
 }
 
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('halemba_model.gltf', (gltf) => {
-    const root = gltf.scene;
-    root.position.set(0, 0, 0);
-    scene.add(root);
-    cameraAnimating = true;
-});
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 2); 
-scene.add(ambientLight);
-
-const sphereGeometry = new THREE.SphereGeometry(0.01, 16, 16); 
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-const cloudPoints = 1000; 
-for (let i = 0; i < cloudPoints; i++) {
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(30));
-    sphere.position.set(x, y, z);
-
-  
-    scene.add(sphere);
-}
-
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-
-    if (cameraAnimating) {
-        camera.position.z += 0.03;
-        camera.position.x += 0.03;
-        camera.position.y += 0.03;
-        if (camera.position.z > 12) {
-            cameraAnimating = false; 
-            showText();
-        }
-    }
-
-    renderer.render(scene, camera);
-}
-
-
-animate();
 window.addEventListener('resize', onWindowResize, false);
+
+function addSceneSwitchButton() {
+    const button = document.createElement('button');
+    button.innerHTML = 'Przełącz scenę';
+    button.style.position = 'absolute';
+    button.style.top = '20px';
+    button.style.left = '20px';
+    button.style.zIndex = 1000;
+    document.body.appendChild(button);
+
+    button.addEventListener('click', () => {
+        if (currentScene === 'main') {
+            switchScene('game');
+        } else {
+            switchScene('main');
+        }
+    });
+}
+
+
+initMainScene();
+addSceneSwitchButton();
+animate();
